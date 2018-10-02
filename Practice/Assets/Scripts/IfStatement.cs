b989c4078b3e7daeb60cd308ce8b84ac6ded8493
Shelby <shelbyyoung003@gmail.com>
2018-10-02 11:33:29 -0600

    Homework - If Statements

---
+++ Practice/Assets/Scripts/IfStatement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class IfStatement : MonoBehaviour
+{
+
+	public FloatData CoinValue, GoldValue;
+	
+	void Update () {
+
+		if (CoinValue.Value > GoldValue.Value)
+		{
+			print("Coin Added to Inventory.");
+		}
+		else
+		{
+			print("Gold Added to Inventory.");
+		}
+		
+	}
+}
