46c0fcf54b3b940dfe5cc12eab8ebea4e7946fd3
Shelby <shelbyyoung003@gmail.com>
2018-10-04 12:47:23 -0600

    Character Mover Practice

---
+++ Practice/Assets/Scripts/CharacterMover.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMover : MonoBehaviour
+{
+
+	private CharacterController controller;
+
+	public float Gravity = 9.81f;
+	public float MoveSpeed = 3.0f;
+	public float JumpSpeed = 3.0f;
+
+	private Vector3 position;
+	private Vector3 rotation;
+	
+	void Start ()
+	{
+
+		controller = GetComponent<CharacterController>();
+		
+	}
+	
+	
+	void Update () 
+	{
+
+		if (controller.isGrounded)
+		{
+			position.Set(0, 0, MoveSpeed * Input.GetAxis("Vertical"));
+			rotation.Set(0, Input.GetAxis("Horizontal"), 0);
+			transform.Rotate(rotation);
+			position = transform.TransformDirection(position);
+
+			if (Input.GetButton("Jump"))
+			{
+				position.y = JumpSpeed;
+			}
+		}
+
+		position.y -= Gravity * Time.deltaTime;
+		controller.Move(position * Time.deltaTime);
+	}
+}
