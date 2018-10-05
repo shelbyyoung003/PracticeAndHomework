5fe1858b9910ccd01af14c54be907cf2acc8ef68
Shelby <shelbyyoung003@gmail.com>
2018-10-05 10:41:08 -0600

    Added PowerUp

---
+++ Practice/Assets/Scripts/AddValue.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+
+public class AddValue : ScriptableObject
+{
+
+	public FloatData Data;
+    
+	public void OnAddValue (FloatData outsideData)
+	{
+		outsideData.Value += Data.Value;
+	}
+
+}
