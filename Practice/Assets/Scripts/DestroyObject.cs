7f0f3b2f9c44415ee70cd4fd6fe7b62783cd491d
Shelby <shelbyyoung003@gmail.com>
2018-10-09 12:14:53 -0600

    Added Ammo

---
+++ Practice/Assets/Scripts/DestroyObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObject : MonoBehaviour {
+
+
+	IEnumerator Start ()
+	{
+
+		yield return new WaitForSeconds(3);
+		Destroy(gameObject);
+
+	}
+
+	private void OnCollisionEnter(Collision other)
+	{
+		Destroy(gameObject);
+		Destroy(other.gameObject);
+	}
+}
---
+++ Practice/Assets/Scripts/Instancing.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instancing : MonoBehaviour
+{
+
+	public GameObject Instance;
+
+	
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.X))
+		{
+			Instantiate(Instance, transform.position, transform.rotation);
+		}
+		
+	}
+}
