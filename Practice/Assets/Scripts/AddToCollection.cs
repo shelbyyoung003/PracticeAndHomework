1650ce33ddac35d154fdc344794b3933b90917a8
Shelby <shelbyyoung003@gmail.com>
2018-10-16 22:30:30 -0600

    Homework - Collections

---
+++ Practice/Assets/Scripts/AddToCollection.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class AddToCollection : ScriptableObject
+{
+
+    public ObjectCollection Collection;
+
+    public void Call(ScriptableObject obj)
+    {
+        Collection.ObjectList.Add(obj);
+    }
+
+}
